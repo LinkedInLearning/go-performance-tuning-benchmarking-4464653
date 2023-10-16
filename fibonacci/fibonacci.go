b64fb685ba64eff801afb3dfee66ab41689692de
ayan george <ayan@ayan.net>
2023-10-16 07:33:23 -0400

    add fib

---
+++ fibonacci/fibonacci.go
@@ -0,0 +1,24 @@
+package fibonacci
+
+type Sequence struct {
+	n int
+}
+
+func (s *Sequence) fib(n int) int {
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	}
+	return s.fib(n-1) + s.fib(n-2)
+}
+
+func (s *Sequence) Next() int {
+	s.n++
+	return s.fib(s.n)
+}
+
+func (s *Sequence) N(n int) int {
+	return s.fib(n)
+}
